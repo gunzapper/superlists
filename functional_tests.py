2cde3157db410e5f501f3e4b50a65a91987a2e2c
gunzapper <gunzapper@gmail.com>
2014-04-03 23:45:49 +0200

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -13,8 +13,12 @@ class NewVisitorTest(unittest.TestCase):
 
 	def test_can_start_a_list_and_retrieve_it_later(self):		
 		self.browser.get('http://localhost:8000')
-		header_text = self.browser.find_element_by_tag_name('h1').text
+
 		self.assertIn('To-Do', self.browser.title)
+
+		
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(
@@ -26,11 +30,11 @@ class NewVisitorTest(unittest.TestCase):
 
 		inputbox.send_keys(Keys.ENTER)
 
-		table = self.browser,find_element_by_id('id_list_table')
+		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)
-			
+			any(row.text == '1: Buy peacock feathers' for row in rows),
+			"New to-do item did not appper in table"			
 		)
 
 
